e6a672710c639fb5c250909dc7de52047f4aecbe
PArangSae_KoreanCoder_한국코더 <72248972+PArangSae@users.noreply.github.com>
2020-10-03 16:39:05 +0900

    Create MoveTo.js

---
+++ MoveTo.js
@@ -0,0 +1,4 @@
+console.log(`Move to anywhere! MoveTo.`)
+urlurl = prompt("What site are you gonna go?")
+alert("If you click 'Okay', you will move to '" + urlurl + "'."")
+window.location.href = urlurl
